099ec925ac73332d048a0614bb45687f2aad3133
pisoftgit <pisoftgit@gmail.com>
2026-01-13 09:40:18 +0530

    property search filter

---
+++ BaseURL.jsx
@@ -0,0 +1,3 @@
+// export const BaseURL = "https://project.pisofterp.com/realestate/api/v1"
+export const BaseURL = "http://localhost:8002/realEstate/api/v1"
+export const BaseURL2 = "http://localhost:8002/realEstate/public/api"
