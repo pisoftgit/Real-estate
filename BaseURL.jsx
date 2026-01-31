e1fbb7211d6843cc26e5e4c55854bd750837a77e
pisoftgit <pisoftgit@gmail.com>
2026-01-31 17:03:13 +0530

    baseurl

--- BaseURL.jsx
+++ BaseURL.jsx
@@ -1,3 +1,3 @@
 export const BaseURL = "https://project.pisofterp.com/realestate/api/v1"
-export const BaseURL3 = "https://project.pisofterp.com/realestate/api"
+export const BaseURL3 = "https://project.pisofterp.com/realestate/"
 export const BaseURL2 = "https://project.pisofterp.com/realestate/public/api"
